8a6e9b509b634c63cf27b2bcfb9ce78dda7f6065
Marten Deinum <mdeinum@gmail.com>
2011-09-27 08:36:06 +0200

    Renamed app to pizzas-r-us. Added notes.txt

--- pizzas-r-us/src/main/java/com/apress/prospringmvc/pizzarus/config/WebMvcConfig.java
+++ pizzas-r-us/src/main/java/com/apress/prospringmvc/pizzarus/config/WebMvcConfig.java
@@ -17,7 +17,7 @@ import org.springframework.web.servlet.handler.WebRequestHandlerInterceptorAdapt
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
 @Configuration
-@EnableWebMvc
+@EnableWebMvc()
 public class WebMvcConfig extends WebMvcConfigurerAdapter {
 
 	@Bean
