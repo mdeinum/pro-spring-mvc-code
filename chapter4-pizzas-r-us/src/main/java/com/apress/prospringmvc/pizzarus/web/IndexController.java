90848b1fab0aff8715319b00582ed0ec2f9a8e67
Marten Deinum <mdeinum@gmail.com>
2011-12-09 15:13:23 +0100

    Sample for chapter 4.

---
+++ chapter4-pizzas-r-us/src/main/java/com/apress/prospringmvc/pizzarus/web/IndexController.java
@@ -0,0 +1,14 @@
+package com.apress.prospringmvc.pizzarus.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class IndexController {
+
+	@RequestMapping(value = "/index.htm")
+	public ModelAndView indexPage() {
+		return new ModelAndView("/WEB-INF/view/index.jsp");
+	}
+}
