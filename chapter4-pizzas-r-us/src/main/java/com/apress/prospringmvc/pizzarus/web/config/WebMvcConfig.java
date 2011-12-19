790fea1e92311b43b82bda0ddb7d339aa702706a
Marten Deinum <mdeinum@gmail.com>
2011-12-19 16:19:46 +0100

    Chapter4. Work in Progress.

--- chapter4-pizzas-r-us/src/main/java/com/apress/prospringmvc/pizzarus/web/config/WebMvcConfig.java
+++ chapter4-pizzas-r-us/src/main/java/com/apress/prospringmvc/pizzarus/web/config/WebMvcConfig.java
@@ -5,6 +5,11 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 /**
@@ -13,12 +18,29 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
  * @author M. Deinum
  */
 @Configuration
+@EnableWebMvc
 @EnableAspectJAutoProxy
 @ComponentScan(basePackages = { "com.apress.prospringmvc.pizzarus.web", "com.apress.prospringmvc.aop" })
-public class WebMvcConfig {
+public class WebMvcConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/css/*").addResourceLocations("classpath:/META-INF/css/**");
+        registry.addResourceHandler("/images/*").addResourceLocations("classpath:/META-INF/images/**");
+    }
+
+    @Override
+    public void addViewControllers(final ViewControllerRegistry registry) {
+        registry.addViewController("/index.htm").setViewName("index");
+    }
+
+    @Override
+    public void configureDefaultServletHandling(final DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
 
     @Bean
-    public ViewResolver internalViewResolver() {
+    public ViewResolver internalResourceViewResolver() {
         InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
         viewResolver.setPrefix("/WEB-INF/view/");
         viewResolver.setSuffix(".jsp");
--- chapter4-pizzas-r-us/src/main/webapp/WEB-INF/view/index.jsp
+++ chapter4-pizzas-r-us/src/main/webapp/WEB-INF/view/index.jsp
@@ -2,10 +2,13 @@
 <%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <html>
 <head>
+
 <title>Welcome to Pizzas R Us</title>
+<link rel="stylesheet" type="text/css" href="<c:url value="/css/pizzas.css"/>" >
 </head>
 <body>
     <header>
+        <img src="<c:url value="/images/slice_of_pizza.jpg"/>" align="right" alt="Slice of Pizza" height="200"/>    
         <h1>Welcome to Pizzas-R-Us. Your online Pizza Store</h1>
         <h2>Home</h2>
     </header>
