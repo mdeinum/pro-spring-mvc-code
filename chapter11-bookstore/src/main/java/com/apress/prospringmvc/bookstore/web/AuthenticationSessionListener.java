16e5e78c5fff3d56f5faa4e0a755c4082808c7d2
Koen Serneels <koen.serneels@skynet.be>
2012-01-23 23:01:14 +0100

    SWF rework

--- chapter11-bookstore/src/main/java/com/apress/prospringmvc/bookstore/web/AuthenticationSessionListener.java
+++ chapter11-bookstore/src/main/java/com/apress/prospringmvc/bookstore/web/AuthenticationSessionListener.java
@@ -4,25 +4,24 @@ import javax.servlet.annotation.WebListener;
 import javax.servlet.http.HttpSessionEvent;
 import javax.servlet.http.HttpSessionListener;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.web.context.support.WebApplicationContextUtils;
 
 import com.apress.prospringmvc.bookstore.service.AccountService;
 import com.apress.prospringmvc.bookstore.service.AuthenticationException;
 
-@Component
 @WebListener
 public class AuthenticationSessionListener implements HttpSessionListener {
 
 	public static final String AUTHENTICATED_ACCOUNT_KEY = "authenticatedAccount";
 
-	@Autowired
-	private AccountService accountService;
-
 	@Override
-	public void sessionCreated(HttpSessionEvent se) {
+	public void sessionCreated(HttpSessionEvent httpSessionEvent) {
+		AccountService accountService = WebApplicationContextUtils.getWebApplicationContext(
+				httpSessionEvent.getSession().getServletContext()).getBean(AccountService.class);
+
 		try {
-			se.getSession().setAttribute(AUTHENTICATED_ACCOUNT_KEY, accountService.login("jd", "secret"));
+			httpSessionEvent.getSession().setAttribute(AUTHENTICATED_ACCOUNT_KEY, accountService.login("jd", "secret"));
 		} catch (AuthenticationException authenticationException) {
 			throw new RuntimeException(authenticationException);
 		}
--- chapter11-bookstore/src/main/java/com/apress/prospringmvc/bookstore/web/config/WebMvcContextConfiguration.java
+++ chapter11-bookstore/src/main/java/com/apress/prospringmvc/bookstore/web/config/WebMvcContextConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.Profile;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.ViewResolver;
