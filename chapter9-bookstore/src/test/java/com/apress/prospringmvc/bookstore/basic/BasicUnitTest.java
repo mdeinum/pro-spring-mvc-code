1397478375a0d13460c5162c6241623685602bc5
Koen Serneels <koen.serneels@skynet.be>
2012-02-13 18:03:19 +0100

    Refactor

--- chapter9-bookstore/src/test/java/com/apress/prospringmvc/bookstore/basic/BasicUnitTest.java
+++ chapter9-bookstore/src/test/java/com/apress/prospringmvc/bookstore/basic/BasicUnitTest.java
@@ -9,10 +9,12 @@ import java.util.List;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.apress.prospringmvc.bookstore.dao.DatabaseDao;
+
 public class BasicUnitTest {
 
     private List<String> database;
-    private Query query;
+    private DatabaseDao databaseDao;
 
     @Before
     public void dataSetup() {
@@ -20,61 +22,31 @@ public class BasicUnitTest {
         for (int i = 0; i < 20; i++) {
             database.add("test" + i);
         }
-        query = new Query(database);
+        databaseDao = new DatabaseDao(database);
     }
 
     @Test
     public void testDeleteQuery() {
-        query.delete("test0");
-
+        assertEquals(1, databaseDao.find("test0").size());
+        databaseDao.delete("test0");
         assertEquals(19, database.size());
-        for (int i = 0; i < 19; i++) {
-            assertEquals("test" + (i + 1), database.get(i));
-        }
+        assertEquals(0, databaseDao.find("test0").size());
     }
 
     @Test
     public void testAddQuery() {
-        query.add("test20");
-
+        assertEquals(0, databaseDao.find("test20").size());
+        databaseDao.add("test20");
         assertEquals(21, database.size());
-        for (int i = 0; i < 21; i++) {
-            assertEquals("test" + i, database.get(i));
-        }
+        assertEquals(1, databaseDao.find("test20").size());
     }
 
     @Test
     public void testFindQuery() {
-        List<String> results = query.find("2");
+        List<String> results = databaseDao.find("2");
         assertEquals(2, results.size());
         for (String result : results) {
             assertTrue(result.equals("test2") || result.equals("test12"));
-        }
-    }
-
-    private class Query {
-        private List<String> database;
-
-        public Query(List<String> database) {
-            this.database = database;
-        }
-
-        public void delete(String element) {
-            database.remove(element);
-        }
-
-        public void add(String element) {
-            database.add(element);
-        }
-
-        public List<String> find(String queryWord) {
-            List<String> result = new ArrayList<String>();
-            for (String element : database) {
-                if (element.contains(queryWord)) {
-                    result.add(element);
-                }
-            }
-            return result;
         }
     }
 }
