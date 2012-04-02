77555c399b59858463640ae2a5b38cb77f240182
Koen Serneels <koen.serneels@skynet.be>
2012-04-02 21:41:24 +0200

    d

---
+++ chapter9-bookstore/src/test/java/com/apress/prospringmvc/bookstore/web/controller/SpringMvcTestLoginControllerTest.java
@@ -0,0 +1,81 @@
+package com.apress.prospringmvc.bookstore.web.controller;
+
+import static org.springframework.test.web.server.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.server.result.MockMvcResultMatchers.redirectedUrl;
+import static org.springframework.test.web.server.result.MockMvcResultMatchers.request;
+import static org.springframework.test.web.server.result.MockMvcResultMatchers.status;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.mockito.internal.verification.VerificationModeFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.server.MockMvc;
+import org.springframework.test.web.server.setup.MockMvcBuilders;
+
+import com.apress.prospringmvc.bookstore.domain.Account;
+import com.apress.prospringmvc.bookstore.domain.support.AccountBuilder;
+import com.apress.prospringmvc.bookstore.service.AccountService;
+import com.apress.prospringmvc.bookstore.service.AuthenticationException;
+import com.apress.prospringmvc.bookstore.web.interceptor.SecurityHandlerInterceptor;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration
+public class SpringMvcTestLoginControllerTest {
+
+    @Autowired
+    private LoginController loginController;
+
+    @Autowired
+    private AccountService accountService;
+
+    private Account account;
+
+    @Before
+    public void setup() throws Exception {
+        account = new AccountBuilder() {
+            {
+                address("Herve", "4650", "Rue de la station", "1", null, "Belgium");
+                credentials("john", "secret");
+                name("John", "Doe");
+            }
+        }.build(true);
+
+        Mockito.when(accountService.login("john", "secret")).thenReturn(account);
+    }
+
+    @Test
+    public void testHandleLogin() throws Exception {
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(loginController).build();
+        mockMvc.perform(post("/login").param("username", "john").param("password", "secret"))
+                .andExpect(status().isOk())
+                .andExpect(request().sessionAttribute(SecurityHandlerInterceptor.ACCOUNT_ATTRIBUTE, account))
+                .andExpect(redirectedUrl("/index.htm"));
+    }
+
+    @After
+    public void verify() throws AuthenticationException {
+        Mockito.verify(accountService, VerificationModeFactory.times(1)).login("john", "secret");
+        Mockito.reset();
+    }
+
+    @Configuration
+    static class LoginControllerTestConfiguration {
+
+        @Bean
+        public AccountService accountService() {
+            return Mockito.mock(AccountService.class);
+        }
+
+        @Bean
+        public LoginController loginController() {
+            return new LoginController();
+        }
+    }
+}
