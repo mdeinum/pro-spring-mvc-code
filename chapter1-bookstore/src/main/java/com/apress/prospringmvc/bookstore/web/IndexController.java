da6d61332798f5549e0be5a91ae4d0643b860b77
Marten Deinum <mdeinum@gmail.com>
2012-04-29 20:07:21 +0200

    Chapter 1, Javadoc.

--- chapter1-bookstore/src/main/java/com/apress/prospringmvc/bookstore/web/IndexController.java
+++ chapter1-bookstore/src/main/java/com/apress/prospringmvc/bookstore/web/IndexController.java
@@ -4,13 +4,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+/**
+ * Controller shows the index page.
+ * 
+ * @author Marten Deinum
+ * @author Koen Serneels
+ */
 @Controller
 public class IndexController {
 
     @RequestMapping(value = "/index.htm")
     public ModelAndView indexPage() {
-    	ModelAndView mav = new ModelAndView("/WEB-INF/views/index.jsp");
-    	mav.addObject("theModelKey", "Spring says HI!");
+        ModelAndView mav = new ModelAndView("/WEB-INF/views/index.jsp");
+        mav.addObject("theModelKey", "Spring says HI!");
         return mav;
     }
 }
